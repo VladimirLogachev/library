389f3c7ed1bac3b6222e54c83b90e856b985a459
Vladimir Logachev <logachev.dev@ya.ru>
2020-10-09 20:50:49 +0700

    Add comments on db

--- db/1.init.sql
+++ db/1.init.sql
@@ -1,11 +1,26 @@
 CREATE TABLE IF NOT EXISTS authors (
     id serial NOT NULL PRIMARY KEY,
-    "name" VARCHAR NOT NULL
+    "name" varchar NOT NULL,
+    UNIQUE ("name")
 );
+
+COMMENT ON TABLE authors IS 'Book authors';
+
+COMMENT ON COLUMN authors.name IS 'Author name';
 
 CREATE TABLE IF NOT EXISTS books (
     id serial NOT NULL PRIMARY KEY,
-    "title" VARCHAR NOT NULL,
-    "cover_image_filename" VARCHAR NOT NULL,
-    "author_id" INTEGER REFERENCES authors (id) NOT NULL
+    "title" varchar NOT NULL,
+    "cover_image_filename" varchar NOT NULL,
+    "author_id" integer REFERENCES authors (id) NOT NULL,
+    UNIQUE ("title", "author_id")
 );
+
+COMMENT ON TABLE books IS 'Books';
+
+COMMENT ON COLUMN books.title IS 'Book title';
+
+COMMENT ON COLUMN books.author_id IS 'Book author';
+
+COMMENT ON COLUMN books.cover_image_filename IS 'Book cover image source file name (path in a bucket, not url)';
+
