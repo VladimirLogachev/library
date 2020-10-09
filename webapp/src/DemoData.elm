48f3b051d3be2e0484e59e42050f8b17cd78e5d6
Vladimir Logachev <logachev.dev@ya.ru>
2020-10-10 03:40:51 +0700

    Add few more books

--- webapp/src/DemoData.elm
+++ webapp/src/DemoData.elm
@@ -17,10 +17,8 @@ loadDemoData =
     Task.sequence
         [ createAuthorAndBooks
             (AuthorInput "Айн Рэнд")
-            [ BookInput "Ночью 16 января; Идеал; Подумай дважды" "three_plays_x2.jpg" 1
-            , BookInput "Источник" "the_fountainhead_x2.jpg" 1
+            [ BookInput "Источник" "the_fountainhead_x2.jpg" 1
             , BookInput "Атлант расправил плечи" "atlas_shrugged_x2.jpg" 1
-            , BookInput "Гимн" "anthem_x2.jpg" 1
             ]
         , createAuthorAndBooks
             (AuthorInput "Алекс Бэнкс, Ева Порселло")
@@ -29,6 +27,18 @@ loadDemoData =
         , createAuthorAndBooks
             (AuthorInput "Евгений Моргунов")
             [ BookInput "PostgreSQL. Основы языка SQL" "postgres_x2.jpg" 3
+            ]
+        , createAuthorAndBooks
+            (AuthorInput "Фредерик Лалу")
+            [ BookInput "Открывая организации будущего" "reinventing_organizations_x2.webp" 4
+            ]
+        , createAuthorAndBooks
+            (AuthorInput "Дэйв Логан, Джон Кинг, Хэли Фишер-Райт")
+            [ BookInput "Лидер и племя" "leader_x2.webp" 5
+            ]
+        , createAuthorAndBooks
+            (AuthorInput "Vitaly Bragilevsky")
+            [ BookInput "Haskell in Depth" "haskell_in_depth_x2.webp" 6
             ]
         ]
         |> Task.mapError (Graphql.Http.mapError <| always ())
